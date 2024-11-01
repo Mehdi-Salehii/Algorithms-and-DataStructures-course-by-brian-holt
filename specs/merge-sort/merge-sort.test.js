ede68f199593dd046e0db12fabe67ecd0d15cce4
Mehdi-Salehii <mehdisalehi9626@gmail.com>
2024-11-01 15:45:32 +0330

    recursion sort / merge sort initial

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -6,13 +6,11 @@
   You'll need to write more than just one function
 */
 
-const mergeSort = (nums) => {
-  // code goes here
-};
+const mergeSort = (nums) => {};
 
 // unit tests
 // do not modify the below code
-test.skip("merge sort", function () {
+test("merge sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
