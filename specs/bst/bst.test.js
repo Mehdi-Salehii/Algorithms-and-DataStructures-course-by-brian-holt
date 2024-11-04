f19c340993246d0fe0121b4ea120f8682c603c02
Mehdi-Salehii <mehdisalehi9626@gmail.com>
2024-11-04 22:48:58 +0330

    binary search initial

--- specs/bst/bst.test.js
+++ specs/bst/bst.test.js
@@ -16,18 +16,16 @@ right - Node/object - the right node which itself may be another tree
 
 */
 
-class Tree {
-  // code goes here
-}
+class Tree {}
 
 // you might consider using a Node class too
-// class Node {
-//   // code maybe goes here
-// }
+class Node {
+  // code maybe goes here
+}
 
 // unit tests
 // do not modify the below code
-describe.skip("Binary Search Tree", function () {
+describe("Binary Search Tree", function () {
   it("creates a correct tree", () => {
     const nums = [3, 7, 4, 6, 5, 1, 10, 2, 9, 8];
     const tree = new Tree();
